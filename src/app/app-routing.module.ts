7afd22bb6096cfb7f4cbf1a9044cca6d447ef11f
sem1colon <vamsikaza@outlook.in>
2020-03-31 03:51:53 +0530

    updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import { EditorComponent } from './editor/editor.component';
+import { ErrorComponent } from './error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", canActivate: [AuthGuard], component: EditorComponent },
+  { path: "login", component: LoginComponent },
+  // { path: "signup", component: SignupComponent },
+  { path: "editor", canActivate: [AuthGuard], component: EditorComponent },
+  { path: "error", component: ErrorComponent },
+  { path: "**", component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EditorComponent } from './editor/editor.component';
+import { HeaderComponent } from './header/header.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EditorComponent,
+    HeaderComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
